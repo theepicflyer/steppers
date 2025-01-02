413176b277a84bf612081b286982969983efcde4
Xie Jiacheng <xiejc2000@gmail.com>
2025-01-03 00:46:39 +0800

    Add comm check

--- simple_test/simple_test.ino
+++ simple_test/simple_test.ino
@@ -3,43 +3,88 @@
 // LilyGo ESP32 UART configuration
 const long SERIAL_BAUD_RATE = 115200;
 
-const int RX_PIN = 15;  // LilyGo UART RX pin
-const int TX_PIN = 33;  // LilyGo UART TX pin, tested with pins 17 and 13. 15 doesnt work
+// UART Pins
+const int UART1_RX = 26;  // LilyGo UART1 RX pin
+const int UART1_TX = 17;  // LilyGo UART1 TX pin
+
+const int UART2_RX = 27;  // LilyGo UART2 RX pin
+const int UART2_TX = 13;  // LilyGo UART2 TX pin
 
 // Motor parameters
 const int32_t RUN_VELOCITY = 17000; // 36000 steps per period * 2 seconds is 1 revolution.
 const int32_t STOP_VELOCITY = 0;
 
 // Test specific parameters
-const int RUN_DURATION = 2000;
-const int STOP_DURATION = 2000;
-const uint8_t RUN_CURRENT_PERCENT = 100;  
-int mode = 0;
+const int DURATION = 2000;
+const uint8_t RUN_CURRENT_PERCENT = 70;  
 
 // Init global variables
-HardwareSerial &serial_stream = Serial2;
-TMC2209 stepper_driver;
+HardwareSerial &serial_stream_1 = Serial1;
+TMC2209 driver1;
+HardwareSerial &serial_stream_2 = Serial2;
+TMC2209 driver2;
 
 void setup() {
     // Initialize Serial for debugging
     Serial.begin(SERIAL_BAUD_RATE);
     
-    // Setup TMC2209 with specific pins and address
-    stepper_driver.setup(serial_stream, SERIAL_BAUD_RATE, TMC2209::SERIAL_ADDRESS_0, RX_PIN, TX_PIN);
+    // Setup TMC2209 
+    driver1.setup(serial_stream1, SERIAL_BAUD_RATE, TMC2209::SERIAL_ADDRESS_0, UART1_RX, UART1_TX);
+    driver1.setRunCurrent(RUN_CURRENT_PERCENT);
+    driver1.enableAutomaticCurrentScaling();
+    driver1.enableCoolStep();
+    driver1.enable();
 
-    stepper_driver.setRunCurrent(RUN_CURRENT_PERCENT);
-    stepper_driver.enableAutomaticCurrentScaling();
-    stepper_driver.enableCoolStep();
-    stepper_driver.enable();
+    driver2.setup(serial_stream2, SERIAL_BAUD_RATE, TMC2209::SERIAL_ADDRESS_0, UART2_RX, UART2_TX);
+    driver2.setRunCurrent(RUN_CURRENT_PERCENT);
+    driver2.enableAutomaticCurrentScaling();
+    driver2.enableCoolStep();
+    driver2.enable();
 
-    stepper_driver.setStandstillMode(stepper_driver.FREEWHEELING);
+    // driver1.setStandstillMode(driver1.FREEWHEELING);
 }
 
 void loop() {
-    mode = (mode + 1) % 5;
-    stepper_driver.moveAtVelocity(RUN_VELOCITY);
-    delay(RUN_DURATION);
+    if (driver1.isSetupAndCommunicating())
+    {
+        Serial.println("Stepper driver is setup and communicating!");
+        Serial.println("Try turning driver power off to see what happens.");
+    }
+    else if (driver1.isCommunicatingButNotSetup())
+    {
+        Serial.println("Stepper driver is communicating but not setup!");
+        Serial.println("Running setup again...");
+        driver1.setup(serial_stream1, SERIAL_BAUD_RATE, TMC2209::SERIAL_ADDRESS_0, UART1_RX, UART1_TX);
+    }
+    else
+    {
+        Serial.println("Stepper driver is not communicating!");
+        Serial.println("Try turning driver power on to see what happens.");
+    }
 
-    stepper_driver.moveAtVelocity(STOP_VELOCITY);
-    delay(STOP_DURATION);
+    if (driver2.isSetupAndCommunicating())
+    {
+        Serial.println("Stepper driver is setup and communicating!");
+        Serial.println("Try turning driver power off to see what happens.");
+    }
+    else if (driver2.isCommunicatingButNotSetup())
+    {
+        Serial.println("Stepper driver is communicating but not setup!");
+        Serial.println("Running setup again...");
+        driver2.setup(serial_stream2, SERIAL_BAUD_RATE, TMC2209::SERIAL_ADDRESS_0, UART2_RX, UART2_TX);
+    }
+    else
+    {
+        Serial.println("Stepper driver is not communicating!");
+        Serial.println("Try turning driver power on to see what happens.");
+    }
+
+    Serial.println();
+    driver1.moveAtVelocity(RUN_VELOCITY);
+    driver2.moveAtVelocity(STOP_VELOCITY);
+    delay(DURATION);
+
+    driver1.moveAtVelocity(STOP_VELOCITY);
+    driver2.moveAtVelocity(RUN_VELOCITY);
+    delay(DURATION);
 }
